2e727f7998b36e44f7e3e5172d6dcdaf90e98fa1
Renato Santos <renatossnt@gmail.com>
2024-06-29 16:48:08 -0300

    feat: modal with confirmation that the changes have been saved

---
+++ frontend/src/adapters/ui/ConfirmModal.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+    Dialog,
+    DialogContent,
+    DialogActions,
+    Typography,
+    Button,
+} from "@mui/material";
+
+export const ConfirmModal = ({ open, handleClose, handleConfirm }) => {
+    return (
+        <Dialog open={open} onClose={handleClose}>
+            <DialogContent>
+                <Typography variant="h6" component="p" gutterBottom>
+                    Deseja confirmar?
+                </Typography>
+            </DialogContent>
+            <DialogActions>
+                <Button
+                    onClick={handleClose}
+                    color="secondary"
+                    variant="contained"
+                >
+                    Cancelar
+                </Button>
+                <Button
+                    onClick={handleConfirm}
+                    color="primary"
+                    variant="contained"
+                >
+                    Concluir
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
--- frontend/src/adapters/ui/ServiceModal.tsx
+++ frontend/src/adapters/ui/ServiceModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     Dialog,
     DialogContent,
@@ -13,72 +13,101 @@ import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 import { LocalizationProvider, StaticDatePicker } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import ptBR from "date-fns/locale/pt-BR";
-
+import { ConfirmModal } from "./ConfirmModal";
 export const ServiceModal = ({ open, handleClose, service }) => {
-    const [selectedDate, setSelectedDate] = React.useState(new Date());
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const [confirmOpen, setConfirmOpen] = useState(false);
+    const handleConfirmClose = () => {
+        setConfirmOpen(false);
+    };
 
+    const handleConfirmOpen = () => {
+        setConfirmOpen(true);
+    };
+
+    const handleConfirm = () => {
+        alert("Agendado");
+        setConfirmOpen(false);
+        handleClose();
+    };
     return (
-        <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
-            {service && (
-                <DialogContent>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} sm={4}>
-                            <CardMedia
-                                component="img"
-                                alt={service.title}
-                                height="140"
-                                image={service.image}
-                                title={service.title}
-                                style={{ borderRadius: "8px" }}
-                            />
+        <>
+            <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
+                {service && (
+                    <DialogContent>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={4}>
+                                <CardMedia
+                                    component="img"
+                                    alt={service.title}
+                                    height="140"
+                                    image={service.image}
+                                    title={service.title}
+                                    style={{ borderRadius: "8px" }}
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={8}>
+                                <Typography
+                                    variant="h6"
+                                    component="p"
+                                    gutterBottom
+                                >
+                                    <strong>Serviço:</strong> {service.title}
+                                </Typography>
+                                <Typography
+                                    variant="h6"
+                                    component="p"
+                                    gutterBottom
+                                >
+                                    <strong>Prestador:</strong>{" "}
+                                    {service.provider}
+                                </Typography>
+                                <Button
+                                    variant="contained"
+                                    color="success"
+                                    style={{ marginBottom: "16px" }}
+                                >
+                                    Entrar em contato
+                                </Button>
+                                <Typography
+                                    variant="body2"
+                                    color="textSecondary"
+                                    component="p"
+                                    gutterBottom
+                                >
+                                    <CalendarTodayIcon /> Visualizado 34 vezes
+                                </Typography>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} sm={8}>
-                            <Typography variant="h6" component="p" gutterBottom>
-                                <strong>Serviço:</strong> {service.title}
+                        <Box mt={2}>
+                            <Typography variant="h6" component="h3">
+                                Descrição:
                             </Typography>
-                            <Typography variant="h6" component="p" gutterBottom>
-                                <strong>Prestador:</strong> {service.provider}
+                            <Typography variant="body2" component="p">
+                                {service.description}
                             </Typography>
-                            <Button
-                                variant="contained"
-                                color="success"
-                                style={{ marginBottom: "16px" }}
-                            >
-                                Entrar em contato
-                            </Button>
-                            <Typography
-                                variant="body2"
-                                color="textSecondary"
-                                component="p"
-                                gutterBottom
-                            >
-                                <CalendarTodayIcon /> Visualizado 34 vezes
+                        </Box>
+                        <Box mt={2}>
+                            <Typography variant="h6" component="h3">
+                                Agenda
                             </Typography>
-                        </Grid>
-                    </Grid>
-                    <Box mt={2}>
-                        <Typography variant="h6" component="h3">
-                            Descrição:
-                        </Typography>
-                        <Typography variant="body2" component="p">
-                            {service.description}
-                        </Typography>
-                    </Box>
-                    <Box mt={2}>
-                        <Typography variant="h6" component="h3">
-                            Agenda
-                        </Typography>
-                    </Box>
-                </DialogContent>
-            )}
-            <DialogActions>
-                <Button onClick={handleClose} color="secondary">
-                    Fechar
-                </Button>
-                <Button onClick={() => alert("Agendado")} color="primary">
-                    Agendar
-                </Button>
-            </DialogActions>
-        </Dialog>
+                        </Box>
+                    </DialogContent>
+                )}
+                <DialogActions>
+                    <Button onClick={handleClose} color="secondary">
+                        Fechar
+                    </Button>
+                    <Button onClick={handleConfirmOpen} color="primary">
+                        Agendar
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <ConfirmModal
+                open={confirmOpen}
+                handleClose={handleConfirmClose}
+                handleConfirm={handleConfirm}
+            />
+        </>
     );
 };
