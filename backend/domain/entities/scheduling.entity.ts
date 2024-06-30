c4c238bdbdf07adf3b21c70f20aa1a63dc26186b
nekoraw <michelbarros123456@gmail.com>
2024-06-30 18:17:11 -0300

    fix(backend): rename interfaces and create classes that implements them

--- backend/domain/entities/scheduling.entity.ts
+++ backend/domain/entities/scheduling.entity.ts
@@ -2,7 +2,7 @@ import { ServiceProviderProfile } from "./service-provider-profile.entity";
 import { Service } from "./service.entity";
 import { User } from "./user.entity";
 
-export interface Scheduling {
+export interface IScheduling {
   id: string;
   service: Service;
   serviceId: string;
@@ -13,4 +13,17 @@ export interface Scheduling {
   customerId: string;
   rating: number;
   serviceProviderProfile?: ServiceProviderProfile;
+}
+
+export class Scheduling implements IScheduling {
+  public readonly id: string;
+  public service: Service;
+  public serviceId: string;
+  public scheduledDate: Date;
+  public isCompleted: boolean;
+  public isCanceled: boolean;
+  public customer: User;
+  public customerId: string;
+  public rating: number;
+  public serviceProviderProfile?: ServiceProviderProfile;
 }
