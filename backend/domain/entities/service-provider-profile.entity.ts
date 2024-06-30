92cbcbdd42d62052e985398807001a1eb288f28f
nekoraw <michelbarros123456@gmail.com>
2024-06-30 18:11:49 -0300

    fix(backend): moved logic to the entity domain

--- backend/domain/entities/service-provider-profile.entity.ts
+++ backend/domain/entities/service-provider-profile.entity.ts
@@ -2,13 +2,40 @@ import { Scheduling } from "./scheduling.entity";
 import { Service } from "./service.entity";
 import { User } from "./user.entity";
 
-export interface ServiceProviderProfile {
+export interface IServiceProviderProfile {
   id: string;
   user: User;
   userId: string;
   telephoneNumber: string;
   specialty: string;
   averageRating: number;
-  services: Service[];
-  history: Scheduling[];
+
+  getServices(): Service[];
+  getHistory(): Scheduling[];
+
+  addService(service: Service): ServiceProviderProfile;
+}
+
+export class ServiceProviderProfile implements IServiceProviderProfile {
+  public readonly id: string;
+  public user: User;
+  public userId: string;
+  public telephoneNumber: string;
+  public specialty: string;
+  public averageRating: number;
+  private services: Service[];
+  private history: Scheduling[];
+
+  public addService(service: Service): ServiceProviderProfile {
+    this.services.push(service);
+    return this;
+  }
+
+  public getServices(): Service[] {
+    return this.services;
+  }
+
+  public getHistory(): Scheduling[] {
+    return this.history;
+  }
 }
--- backend/domain/repositories/service-provider-profile-repository.ts
+++ backend/domain/repositories/service-provider-profile-repository.ts
@@ -1,14 +1,9 @@
 import { ServiceProviderProfile } from "../entities/service-provider-profile.entity";
 import { UUID } from "crypto";
-import { Service } from "../entities/service.entity";
 
 export interface ServiceProviderProfileRepository {
   getById(id: UUID): ServiceProviderProfile;
   getByServiceProviderId(serviceProviderId: UUID): ServiceProviderProfile;
-  addService(
-    serviceProviderProfileRepository: ServiceProviderProfile,
-    service: Service,
-  ): ServiceProviderProfile;
   update(serviceProviderProfile: ServiceProviderProfile): void;
   save(serviceProviderProfile: ServiceProviderProfile): void;
 }
