ef2440b951cee792309b6ef36e7e33e3829d9b46
nekoraw <michelbarros123456@gmail.com>
2024-06-30 18:12:30 -0300

    fix(backend): moved logic to the service entity

--- backend/domain/entities/service.entity.ts
+++ backend/domain/entities/service.entity.ts
@@ -1,7 +1,7 @@
 import { Scheduling } from "./scheduling.entity";
 import { ServiceProviderProfile } from "./service-provider-profile.entity";
 
-export interface Service {
+export interface IService {
   id: string;
   title: string;
   description: string;
@@ -10,7 +10,35 @@ export interface Service {
   location: Location;
   createdAt: Date;
   active: boolean;
-  availableDates: Date[];
   creatorProfile: ServiceProviderProfile;
-  schedules: Scheduling[];
+  getAvailableDates(): Date[];
+  getSchedules(): Scheduling[];
+  addAvailableDate(date: Date): Service;
+}
+
+export class Service implements IService {
+  public readonly id: string;
+  public title: string;
+  public description: string;
+  public viewCount: number;
+  public category: string;
+  public location: Location;
+  public createdAt: Date;
+  public active: boolean;
+  public creatorProfile: ServiceProviderProfile;
+  private availableDates: Date[];
+  private schedules: Scheduling[];
+
+  public getAvailableDates(): Date[] {
+    return this.availableDates;
+  }
+
+  public getSchedules(): Scheduling[] {
+    return this.schedules;
+  }
+
+  public addAvailableDate(date: Date) {
+    this.availableDates.push(date);
+    return this;
+  }
 }
