3c4ee52fd74b159850de89e7f65a941801a70afc
nekoraw <michelbarros123456@gmail.com>
2024-07-01 11:36:37 -0300

    feat(backend): change how location is handled

--- backend/domain/repositories/location-repository.ts
+++
@@ -1,8 +0,0 @@
-import { Location } from "../entities/location.entity";
-import { UUID } from "crypto";
-
-export interface ILocationRepository {
-  getById(id: UUID): Location;
-  getByLatitudeLongitude(latitude: number, longitude: number): Location;
-  save(location: Location): void;
-}
