70753a037a871f40634c6be55081ccf847e37bda
Renato Santos <renatossnt@gmail.com>
2024-07-02 18:40:37 -0300

    feat: text as props and color changes in ConfirmModal

--- frontend/src/adapters/ui/ConfirmModal.tsx
+++ frontend/src/adapters/ui/ConfirmModal.tsx
@@ -13,19 +13,20 @@ export const ConfirmModal = ({
   handleClose,
   service,
   handleConfirm,
+  text,
 }: ServiceModalProps) => {
   return (
     <Dialog open={open} onClose={handleClose}>
       <DialogContent>
         <Typography variant="h6" component="p" gutterBottom>
-          Deseja confirmar?
+          {text}
         </Typography>
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose} color="secondary" variant="contained">
+        <Button onClick={handleClose} color="error" variant="text">
           Cancelar
         </Button>
-        <Button onClick={handleConfirm} color="primary" variant="contained">
+        <Button onClick={handleConfirm} color="success" variant="contained">
           Concluir
         </Button>
       </DialogActions>
