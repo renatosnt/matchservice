e84743e21637c054a243883b7bf95bc01aa431b5
nekoraw <michelbarros123456@gmail.com>
2024-07-03 11:05:21 -0300

    docs(api): added documentation for the service router

--- backend/application/express/routes/service-router.ts
+++ backend/application/express/routes/service-router.ts
@@ -23,15 +23,44 @@ const serviceProviderProfileAdapter = new ServiceProviderProfileAdapter(
 );
 const scheduleAdapter = new SchedulingAdapter(new SchedulingDatabase());
 
+/**
+ * @openapi
+ * /service/search:
+ *   get:
+ *     tags:
+ *       - service
+ *     summary: Searches for services in the database.
+ *     requestBody:
+ *       description: The service information. Only exact matches are shown. Properties are OR'd.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               category:
+ *                 type: string
+ *               profileId:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Returns all services that match
+ *       500:
+ *         description: Internal Server Error
+ *
+ */
 router.get("/search", async (req: Request, res: Response) => {
-  const { title, description, category, creatorProfileId } = req.body;
+  const { title, description, category, ProfileId } = req.body;
 
   try {
     const services = await serviceAdapter.search(
       title,
       description,
       category,
-      creatorProfileId,
+      ProfileId,
     );
     res.json(services);
   } catch (error: any) {
@@ -39,6 +68,20 @@ router.get("/search", async (req: Request, res: Response) => {
   }
 });
 
+/**
+ * @openapi
+ * /service/categories:
+ *   get:
+ *     tags:
+ *       - service
+ *     summary: Searches for all unique categories in the database.
+ *     responses:
+ *       200:
+ *         description: Returns all unique categories
+ *       500:
+ *         description: Internal Server Error
+ *
+ */
 router.get("/categories", async (req: Request, res: Response) => {
   try {
     const categories = await serviceAdapter.getUniqueCategories();
@@ -48,6 +91,27 @@ router.get("/categories", async (req: Request, res: Response) => {
   }
 });
 
+/**
+ * @openapi
+ * /service/{serviceId}:
+ *   get:
+ *     tags:
+ *       - service
+ *     summary: Gets the service by it's ID. Adds a view count.
+ *     parameters:
+ *       - in: path
+ *         name: serviceId
+ *         required: true
+ *         schema:
+ *           type: UUID
+ *         description: The service ID.
+ *     responses:
+ *       200:
+ *         description: Returns the service
+ *       500:
+ *         description: Internal Server Error
+ *
+ */
 router.get("/:serviceId", async (req: Request, res: Response) => {
   const serviceId = req.params.serviceId as UUID;
 
@@ -64,6 +128,27 @@ router.get("/:serviceId", async (req: Request, res: Response) => {
   }
 });
 
+/**
+ * @openapi
+ * /service/{serviceId}/schedule:
+ *   get:
+ *     tags:
+ *       - service
+ *     summary: Gets the entire schedule of a service by it's ID.
+ *     parameters:
+ *       - in: path
+ *         name: serviceId
+ *         required: true
+ *         schema:
+ *           type: UUID
+ *         description: The service ID.
+ *     responses:
+ *       200:
+ *         description: Returns the entire schedule
+ *       500:
+ *         description: Internal Server Error
+ *
+ */
 router.get("/:serviceId/schedule", async (req: Request, res: Response) => {
   const serviceId = req.params.serviceId as UUID;
 
@@ -75,6 +160,29 @@ router.get("/:serviceId/schedule", async (req: Request, res: Response) => {
   }
 });
 
+/**
+ * @openapi
+ * /service/{serviceId}:
+ *   delete:
+ *     tags:
+ *       - service
+ *     summary: Deletes a service by it's ID.
+ *     security:
+ *       - JWT: []
+ *     parameters:
+ *       - in: path
+ *         name: serviceId
+ *         required: true
+ *         schema:
+ *           type: UUID
+ *         description: The service ID.
+ *     responses:
+ *       200:
+ *         description: Returns the deleted service
+ *       500:
+ *         description: Internal Server Error
+ *
+ */
 router.delete(
   "/:serviceId",
   sessionMiddleware,
@@ -113,6 +221,44 @@ router.delete(
   },
 );
 
+/**
+ * @openapi
+ * /service/create:
+ *   post:
+ *     tags:
+ *       - service
+ *     summary: Creates a new service.
+ *     security:
+ *       - JWT: []
+ *     requestBody:
+ *       description: The service information.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               category:
+ *                 type: string
+ *               locationState:
+ *                 type: string
+ *               locationCity:
+ *                 type: string
+ *             required:
+ *               - title
+ *               - description
+ *               - category
+ *               - locationState
+ *               - locationCity
+ *       201:
+ *         description: Returns the newly created service
+ *       500:
+ *         description: Internal Server Error
+ *
+ */
 router.post(
   "/create",
   [ContentTypeMiddleware, sessionMiddleware],
@@ -157,6 +303,45 @@ router.post(
   },
 );
 
+/**
+ * @openapi
+ * /service/{serviceId}:
+ *   patch:
+ *     tags:
+ *       - service
+ *     summary: Updates the content of a service.
+ *     security:
+ *       - JWT: []
+ *     parameters:
+ *       - in: path
+ *         name: serviceId
+ *         required: true
+ *         schema:
+ *           type: UUID
+ *         description: The service ID.
+ *     requestBody:
+ *       description: The service information. Only the provided fields are going to be updated.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               category:
+ *                 type: string
+ *               locationState:
+ *                 type: string
+ *               locationCity:
+ *                 type: string
+ *       201:
+ *         description: Returns the updated service
+ *       500:
+ *         description: Internal Server Error
+ *
+ */
 router.patch(
   "/:serviceId",
   [ContentTypeMiddleware, sessionMiddleware],
@@ -165,7 +350,7 @@ router.patch(
     const userData = (req as CustomRequest).userData;
 
     try {
-      const { title, description, category, locationState, locationCity, a } =
+      const { title, description, category, locationState, locationCity } =
         req.body;
       const authenticatedUser = await userAdapter.getById(userData.id);
       const existingService: Service = await serviceAdapter.getById(serviceId);
@@ -181,7 +366,6 @@ router.patch(
       if (category) existingService.category = category;
       if (locationState) existingService.locationState = locationState;
       if (locationCity) existingService.locationCity = locationCity;
-      if (a) existingService.creatorProfileId = a;
 
       const updatedService: Service =
         await serviceAdapter.save(existingService);
