b986dfb795bf793cacbe9cc34f9c263e3e4db1ef
nekoraw <michelbarros123456@gmail.com>
2024-07-03 13:03:27 -0300

    fix(backend): make name easier

--- backend/application/express/routes/login-router.ts
+++ backend/application/express/routes/login-router.ts
@@ -74,7 +74,7 @@ router.post("/", ContentTypeMiddleware, async (req: Request, res: Response) => {
     const token = jwt.sign(userData, SECRET_KEY, {
       expiresIn: "7d",
     });
-    res.status(201).json({ "x-access-token": token,  ...userData });
+    res.status(201).json({ token: token,  ...userData });
     res.end();
   } catch (error: any) {
     res.status(500).json({ message: error.message });
