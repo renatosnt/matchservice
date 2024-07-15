3066707087d03f55055a20ebed8303a8c97c6097
nekoraw <michelbarros123456@gmail.com>
2024-07-15 15:19:57 -0300

    feat(test): add testing and coverage support to the backend

--- backend/intrastructure/mappings/email-validation.ts
+++ backend/intrastructure/mappings/email-validation.ts
@@ -2,6 +2,6 @@ export function validateEmail(parameter: any) {
   const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i;
   if (!regex.test(parameter))
     throw new Error(
-      `${Object.keys({ parameter })[0]} should be a valid e-mailc.`,
+      `The e-mail ${parameter} is invalid.`,
     );
 }
---
+++ backend/intrastructure/mappings/tests/email-validation.test.ts
@@ -0,0 +1,18 @@
+import {validateEmail} from "../email-validation";
+import {describe, expect, test} from '@jest/globals';
+
+describe("tests the email validation", () => {
+    test("tests a correct email", () => {
+        const sampleEmail = "example@example.com";
+        expect(validateEmail(sampleEmail)).toBe(undefined);
+    })
+
+    test("tests a incorrect email", () => {
+        const sampleEmail = "example@.com";
+        function validateWrongEmail(){
+            validateEmail(sampleEmail)
+        }
+        
+        expect(validateWrongEmail).toThrow(new Error(`The e-mail ${sampleEmail} is invalid.`));
+    })
+})
