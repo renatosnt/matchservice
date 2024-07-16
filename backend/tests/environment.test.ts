276ec527904e4ac9c97a310d2bda2d85bf13d9c1
nekoraw <michelbarros123456@gmail.com>
2024-07-16 12:01:31 -0300

    test(enviroment): add new tests

---
+++ backend/tests/environment.test.ts
@@ -0,0 +1,27 @@
+import { getSecretKey, getBackendPort } from "../environment";
+
+describe("tests the environment variables", () => {
+  test("tests if can get the secret if it's set", () => {
+    process.env.SECRET_KEY = "supersecretkey";
+    expect(getSecretKey()).toBe(process.env.SECRET_KEY);
+  });
+
+  test("tests if throws an error if the secret key is not set", () => {
+    delete process.env.SECRET_KEY;
+    expect(getSecretKey).toThrowError(
+      new Error("SECRET_KEY environment variable is not set."),
+    );
+  });
+
+  test("tests if can get the backend port if it's set", () => {
+    process.env.BACKEND_PORT = "2811";
+    expect(getBackendPort()).toBe(process.env.BACKEND_PORT);
+  });
+
+  test("tests if throws an error if the secret key is not set", () => {
+    delete process.env.BACKEND_PORT;
+    expect(getBackendPort).toThrowError(
+      new Error("BACKEND_PORT environment variable is not set."),
+    );
+  });
+});
